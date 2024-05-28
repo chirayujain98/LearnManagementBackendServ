0a0ea059f1bb993ec4e657576f8237e43bf68ccd
chirayujain98 <chirayu.jain98@gmail.com>
2024-05-28 13:42:44 +0530

    Project initialization

---
+++ src/main/java/com/example/learnmanagementbakcendserv/LearnManagementBakcendServApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnmanagementbakcendserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnManagementBakcendServApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnManagementBakcendServApplication.class, args);
+    }
+
+}
